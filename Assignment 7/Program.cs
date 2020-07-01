398563f7c474ffb415ce72fe81a79a0156303dd4
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-07-01 00:36:39 -0600

    Add files via upload

---
+++ Assignment 7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using DryIoc;
+using ImTools;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Assignment_7
+{
+    [ExcludeFromCodeCoverage]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new ServiceCollection()
+                           .AddSingleton<ICalculator, Calculator>()
+                           .BuildServiceProvider();
+
+            var service = provider.GetService<ICalculator>();
+
+            var menuRecall = true;
+
+            var displayMenu = new CalculatorDisplayUtility();
+
+            while (menuRecall)
+            {
+                displayMenu.Menu(out menuRecall, service);
+            }
+        }
+    }
+}
