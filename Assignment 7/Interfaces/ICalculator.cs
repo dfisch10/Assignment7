eae8bd73be41509e7f51d3ed79d2ab93edd3ad83
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-08-02 21:58:48 -0600

    Add files via upload

--- Assignment 7/Interfaces/ICalculator.cs
+++ Assignment 7/Interfaces/ICalculator.cs
@@ -3,40 +3,44 @@
     public interface ICalculator
     {
         #region Parameters
+
         /// <summary>
         /// This parameter is an array of type double which the user inputs and is used for various calculator functions.
         /// </summary>
         double[] Operands { get; set; }
+
         #endregion
 
         #region Methods
+
         /// <summary>
         /// Takes an ICalculator object's operands that the user inputs, and then returns their sum.
         /// </summary>
         /// <param name="operands">An array of double type numeric values.</param>
         /// <returns>Returns the sum of all the numbers.</returns>
-        double Add(ICalculator calculator);
+        double Add(params double[] values);
 
         /// <summary>
         /// Takes an ICalculator object's operands that the user inputs, and then returns the difference.
         /// </summary>
         /// <param name="operands">An array of double type numeric values.</param>
         /// <returns>Returns the difference of all the numbers.</returns>
-        double Subtract(ICalculator calculator);
+        double Subtract(params double[] values);
 
         /// <summary>
         /// Takes an ICalculator object's operands that the user inputs, and then returns their product.
         /// </summary>
         /// <param name="operands">An array of double type numeric values.</param>
         /// <returns>Returns the product of all the numbers.</returns>
-        double Multiply(ICalculator calculator);
+        double Multiply(params double[] values);
 
         /// <summary>
         /// Takes an ICalculator object's operands that the user inputs, and then returns thei quotient.
         /// </summary>
         /// <param name="operands">An array of double type numeric values.</param>
         /// <returns>Returns the quotient of all the numbers.</returns>
-        double Divide(ICalculator calculator);
+        double Divide(params double[] values);
+
         #endregion
     }
 }
